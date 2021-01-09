43cf15a838f9fcbd4b8007bd2868d0251c2e545c
rosin <rosin@DESKTOP-DBFKOIQ.local>
2021-01-09 19:07:36 +0100

    Added Trophy assets; Added Trophy scripts; Modified Planet_Background_Modify.psd;

---
+++ Assets/Scripts/OverworldTrophies.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverworldTrophies : MonoBehaviour
+{
+    public SpriteRenderer graviree;
+    public SpriteRenderer aimAndFire;
+    public SpriteRenderer spacewordetsu;
+    public SpriteRenderer stationJump;
+    public SpriteRenderer shipBoom;
+    public SpriteRenderer spaceDefence;
+    void Start()
+    {
+        if (TrophyManager.Instance.gravireee_Trophy == true)
+        {
+            graviree.enabled = true;
+        }
+        if (TrophyManager.Instance.aimAndFire_Trophy == true)
+        {
+            aimAndFire.enabled = true;
+        }
+        if (TrophyManager.Instance.spacewordetsu_Trophy == true)
+        {
+            spacewordetsu.enabled = true;
+        }
+        if (TrophyManager.Instance.stationJump_Trophy == true)
+        {
+            stationJump.enabled = true;
+        }
+        if (TrophyManager.Instance.shipBoom_Trophy == true)
+        {
+            shipBoom.enabled = true;
+        }
+        if (TrophyManager.Instance.spaceDefence_Trophy == true)
+        {
+            spaceDefence.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/TrophyManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrophyManager : MonoBehaviour
+{
+    public bool gravireee_Trophy; //1
+    public bool aimAndFire_Trophy; //2
+    public bool spacewordetsu_Trophy; //3
+    public bool stationJump_Trophy; //4
+    public bool shipBoom_Trophy; //5
+    public bool spaceDefence_Trophy; //6
+    private static TrophyManager _instance;
+
+    public static TrophyManager Instance
+    {
+        get { return _instance; }
+    }
+
+    private void Start()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            //Debug.Log("Duplicate Deleted:: SceneLoader - There can only be one. Ignore this if the same scene was loaded.");
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void GetTrophy(int level)
+    {
+        switch (level)
+        {
+            case 1:
+                gravireee_Trophy = true;
+                break;
+            case 2:
+                aimAndFire_Trophy = true;
+                break;
+            case 3:
+                spacewordetsu_Trophy = true;
+                break;
+            case 4:
+                stationJump_Trophy = true;
+                break;
+            case 5:
+                shipBoom_Trophy = true;
+                break;
+            case 6:
+                spaceDefence_Trophy = true;
+                break;
+            default:
+                break;
+        }
+    }
+}
